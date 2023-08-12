ebb5a24d0b428db3663da0ba544deca451c2b5ad
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-08-12 10:15:15 +0100

    added more features
    added fade load out animation
    added roboto font
    further implemented modular codes and readability

--- src/scripts/methods.js
+++ src/scripts/methods.js
@@ -1,3 +1,4 @@
+//show components
 export function showElements(elementsArray){
   if (!elementsArray) return;
 
@@ -7,40 +8,13 @@ export function showElements(elementsArray){
     el.style.display = 'block'; //prev display is none
   }
 }
+//position elements at top or bottom or center
+export function positionElement(element, className) {
+  if(!element) return;
 
-export const animatePercentage = (el, speed) => animateElement(el, 'text', 1, 100, speed)
-export const animateProgress = (el, speed) => animateElement(el, 'width', 4, 400, speed)
-
-
-// Add Here >> if adding a new element with a new property type  >>
-function animateProperty(element, type, value) {
-  switch (type) {
-    case "text": 
-      element.textContent = value;
-      break;
-    case "width": 
-      element.style.width = value + 'px';
-      break;
-    default:
-      element.textContent = value;
-      break;
-  }
+  element.classList.add(className)
 }
-// <<<<
 
-function animateElement(element, type, increment, total, animationSpeed){
-  //type indicates either text, width etc
-  if (!element) return;
-  // >>>>>
-  var value = 0;
-  const totalValue = total;
-  const loading = setInterval(animation, animationSpeed);
-
-  //animation
-  function animation() {
-    if (value >= totalValue) return clearInterval(loading);
-    // >>>>>
-    value = value + increment;
-    animateProperty(element, type, value);
-  }
-}
+//background color method
+//text color method
+//font size method
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,37 +1,56 @@
 import { 
-  showElements, 
-  animatePercentage, 
-  animateProgress
+  showElements,
+  positionElement,
 } from "./methods.js";
 
+import {
+  animatePercentage, 
+  animateProgress,
+} from "./animation.js"
 // Elements declarations
 const Elements = {
   percentage: document.querySelector('.percentage'),
   progress: document.querySelector('.progress'),
+  positioner: document.querySelector('.positioner'),
+  background: document.querySelector('.background'),
 }
+
+//loading in delay
+const loadInDelay = 1000;
+//loading out delay
+const loadOutDelay = 500;
 
 function showPreloader(modifications = {}) {
   const elements = modifications['show'];
+  const position = modifications['position'] ?? 'center'
+  const loadOutAnim = modifications['loadOut'] ?? 'fade'
+
   showElements(elements) //show elements
+  positionElement(Elements.positioner, position) //position element
+
   //elements
-  const animationSpeed = 10;
+  const animationSpeed = 10; //lower the number, higher the speed
   for ( let i of elements) {
     const element = Elements[i]
     if (!element) return;
     switch(i) {
       case 'percentage':
-        animatePercentage(element, animationSpeed)
+        animatePercentage(element, Elements.background, loadOutAnim, animationSpeed, loadOutDelay);
+        break;
       case 'progress':
-        animateProgress(element, animationSpeed)
+        animateProgress(element, Elements.background, loadOutAnim, animationSpeed, loadOutDelay);
+        break;
     }
   }
 }
-
+// add color requirement funciton
 setTimeout(()=> {
   showPreloader({
-    'show': ['percentage']
+    'show': ['percentage'],
+    'position': 'bottom-left',
+    'loadOut': 'fade'
   });
-}, 1000)
+}, loadInDelay)
 
 
 // showPreloader({
